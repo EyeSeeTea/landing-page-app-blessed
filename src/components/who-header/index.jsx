5bdb413379616c68120a3d181ba3de0b0507828e
Alexis Rico <sferadev@gmail.com>
2019-08-12 10:31:03 +0000

    Fix styles for header

--- src/components/who-header/index.jsx
+++ src/components/who-header/index.jsx
@@ -1,34 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router-dom";
-import { withStyles, Typography } from "@material-ui/core";
+import { withStyles, Typography, Chip } from "@material-ui/core";
+import { Home, ExitToApp } from "@material-ui/icons";
 
 import { styles } from "./styles";
 import { goToExternalUrl, goToDhis2Url } from "../../utils";
 
 const WHOHeader = ({ classes, history, baseUrl, title, backUrl }) => {
+    const actionWHO = () => goToExternalUrl("https://who.int");
+    const actionBack = () => history.push(backUrl);
+    const actionLogout = () => goToDhis2Url(baseUrl, "/dhis-web-commons-security/logout.action");
+
     return (
-        <header className={classes.header}>
-            <img
-                className={classes.headerLogo}
-                alt={title}
-                src="img/who-logo.png"
-                onClick={() => goToExternalUrl("https://who.int")}
+        <header className={classes.container}>
+            <img className={classes.logo} onClick={actionWHO} alt={title} src="img/who-logo.png" />
+            <div className={classes.titleContainer} onClick={actionBack}>
+                <Home className={classes.title} fontSize="large" />
+                <Typography className={classes.title} variant="h4">
+                    {title}
+                </Typography>
+            </div>
+
+            <Chip
+                icon={<ExitToApp />}
+                label="LOG OUT"
+                className={classes.logout}
+                onClick={actionLogout}
             />
-            <Typography
-                className={classes.headerTitle}
-                variant="h4"
-                onClick={() => history.push(backUrl)}
-            >
-                {title}
-            </Typography>
-            <Typography
-                className={classes.headerLogout}
-                variant="h6"
-                onClick={() => goToDhis2Url(baseUrl, "/dhis-web-commons-security/logout.action")}
-            >
-                {"LOG OUT"}
-            </Typography>
         </header>
     );
 };
--- src/components/who-header/styles.jsx
+++ src/components/who-header/styles.jsx
@@ -1,29 +1,32 @@
 export const styles = _theme => ({
-    header: {
+    container: {
         backgroundColor: "#0072bb",
         transition: "all 0.3s",
         display: "flex",
         justifyContent: "space-between",
         padding: "25px",
     },
-    headerLogo: {
+    logo: {
         verticalAlign: "middle",
         filter: "brightness(0) invert(1)",
         height: "100px",
         cursor: "pointer",
         margin: "auto",
     },
-    headerTitle: {
+    titleContainer: {
+        display: "flex",
+    },
+    title: {
         color: "white",
         lineHeight: "1.5",
         cursor: "pointer",
-        margin: "auto",
-        padding: "25px",
         textAlign: "center",
-    },
-    headerLogout: {
-        cursor: "pointer",
-        color: "white",
         margin: "auto",
+        marginLeft: "15px",
+        marginRight: "15px",
+    },
+    logout: {
+        margin: "auto",
+        padding: "15px",
     },
 });
