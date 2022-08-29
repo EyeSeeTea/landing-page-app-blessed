836dfebd0244ecea96beef0a03952e5e76acc893
Arnau Sanchez <pyarnau@gmail.com>
2022-08-29 13:33:23 +0200

    International projects: only render after checking redirects

--- src/webapp/pages/landing-page/international.jsx
+++ src/webapp/pages/landing-page/international.jsx
@@ -1,4 +1,4 @@
-import { withStyles } from "@material-ui/core";
+import { LinearProgress, withStyles } from "@material-ui/core";
 import React, { useEffect } from "react";
 import _ from "lodash";
 import { styles } from "../../../domain/models/hepatitis/styles";
@@ -7,15 +7,23 @@ import LandingPage from "./generic";
 import { DATA_MANAGEMENT_USER, EFH_USER } from "../../../data/logic/redirection";
 
 const InternationalLandingPage = props => {
+    const [renderPage, setRenderPage] = React.useState(false);
+
     useEffect(() => {
         if (_.intersection(props.userGroupIds, [DATA_MANAGEMENT_USER, EFH_USER]).length < 2) {
             if (_.intersection(props.userGroupIds, [DATA_MANAGEMENT_USER]).length > 0) {
-                return goToDhis2Url(props.baseUrl, "/api/apps/Data-Management-App/index.html");
+                goToDhis2Url(props.baseUrl, "/api/apps/Data-Management-App/index.html");
             } else if (_.intersection(props.userGroupIds, [EFH_USER]).length > 0) {
-                return goToDhis2Url(props.baseUrl, "/api/apps/Emergency-Field-Hospital-App/index.html");
+                goToDhis2Url(props.baseUrl, "/api/apps/Emergency-Field-Hospital-App/index.html");
+            } else {
+                setRenderPage(true);
             }
+        } else {
+            setRenderPage(true);
         }
     }, [props]);
+
+    if (!renderPage) return <LinearProgress />;
 
     return <LandingPage {...props} />;
 };
